14e4cee91870fc7294bcbfd4a65ccd6716f51960
abeatrizdscoelho <anabeatrizdscoelho@gmail.com>
2025-10-03 13:04:56 -0300

    feat: Implementado Atividade II do AutoBots

---
+++ automanager/src/main/java/com/autobots/automanager/entidades/Documento.java
@@ -0,0 +1,22 @@
+package com.autobots.automanager.entidades;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Documento extends RepresentationModel<Documento> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String tipo;
+    @Column(unique = true)
+    private String numero;
+}
